1fdf3c5c567b697ab36b70b28fd6a77ce196967e
IvaniltonsSilva <265127343+IvaniltonsSilva@users.noreply.github.com>
2026-05-16 09:39:48 -0300

    chore: update shareCode to Z0O60LNNLFX

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6e81a4di?shareCode=NJBYMO32BIP&afp=oa' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6e81a4di?shareCode=Z0O60LNNLFX&afp=oa' });
     res.end();
   }
