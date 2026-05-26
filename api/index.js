098cb02cfe9343a88f236a5cd74971b911fca35a
IvaniltonsSilva <265127343+IvaniltonsSilva@users.noreply.github.com>
2026-05-26 12:02:10 -0300

    chore: update shareCode to ZMV2DP4P0WH

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6e81a4di?shareCode=VYU22TH6U14&afp=oa' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6e81a4di?shareCode=ZMV2DP4P0WH&afp=oa' });
     res.end();
   }
