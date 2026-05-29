c2d3e6bb2ef6a2eedd17d44dd031fcbe1a60c4e0
JCPrime01 <vanzokiller@gmail.com>
2026-05-29 11:28:04 -0300

    chore: shareCode BQ7H3A7GE5A -> NAP0SIAYUWH (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6e81a4di?shareCode=BQ7H3A7GE5A&afp=oa' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6e81a4di?shareCode=NAP0SIAYUWH&afp=oa' });
     res.end();
   }
